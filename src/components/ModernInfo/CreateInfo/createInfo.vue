bded97d9b001f70e2a721638155b5fe17cfb5b43
UbuntuRoot <757909414@qq.com>
2022-03-07 17:22:46 +0800

    zjs

--- src/components/ModernInfo/CreateInfo/createInfo.vue
+++ src/components/ModernInfo/CreateInfo/createInfo.vue
@@ -98,7 +98,7 @@ export default {
   },
   beforeRouteEnter(to, from, next) {
     console.log("to", to, "from", from, to.query.id);
-    if (from.path == "/") {
+    if (from.path == "/" && to.query.id) {
       next(async (vm) => {
         let status = 0;
         if (to.query.id) {
@@ -117,6 +117,35 @@ export default {
         }
       });
     }
+    if (from.path == "/" && !to.query.id) {
+      next(async (vm) => {
+        vm.$router.push({
+          path: "/home",
+        });
+      });
+    }
+    //  else {
+    //   next(async (vm) => {
+    //     vm.$router.push({
+    //       path: "/home",
+    //     });
+    // let status = 0;
+    // if (to.query.id) {
+    //   status = await MoJudge(
+    //     {
+    //       task_id: to.query.id,
+    //     },
+    //     vm.$router,
+    //     vm.$store
+    //   );
+    // } else {
+    //   status = 200;
+    // }
+    // if (status != 200) {
+    //   vm.$message.warning("数据出错");
+    // }
+    // });
+    //}
     next();
   },
   methods: {
